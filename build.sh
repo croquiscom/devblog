4e76a22f67b289beacbb343abc8a09866f5e1dd0
Sangmin Yoon <sangmin.yoon@croquis.com>
2012-05-26 06:37:24 +0000

    Add RSS feed link

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 mkdir -p ../homepage/devblog.croquis.com
 
-cp index.html ../homepage/devblog.croquis.com/
+cp index.html favicon.ico ../homepage/devblog.croquis.com/
 
 cd ko
-jekyll --no-auto
+jekyll --no-auto ../../homepage/devblog.croquis.com/ko
 cd ..
 
 #cd en
-#jekyll --no-auto
+#jekyll --no-auto ../../homepage/devblog.croquis.com/en
 #cd ..
