ea7249137715c09eb7826ba905467d09a9d9c29f
Sangmin Yoon <sangmin.yoon@croquis.com>
2014-01-03 15:56:33 +0900

    add build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-mkdir -p ../homepage/devblog.croquis.com
+TARGET=~/_site
 
-cp index.html favicon.ico ../homepage/devblog.croquis.com/
+if [ -n "$1" ]
+then
+  TARGET=$1
+fi
+
+mkdir -p $TARGET/devblog.croquis.com
+
+cp index.html favicon.ico $TARGET/devblog.croquis.com/
 
 cd ko
-jekyll --no-auto ../../homepage/devblog.croquis.com/ko
+jekyll --no-auto $TARGET/devblog.croquis.com/ko
 cd ..
 
 #cd en
-#jekyll --no-auto ../../homepage/devblog.croquis.com/en
+#jekyll --no-auto $TARGET/devblog.croquis.com/en
 #cd ..
