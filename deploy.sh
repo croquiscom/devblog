01a51faf31804d9acd34c00999e4534a554b1b86
Sangmin Yoon <sixmen@gmail.com>
2022-05-13 13:26:18 +0900

    croquis.com -> kakaostyle.com

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,11 @@
 
 ./build.sh
 
-aws --profile croquis s3 sync docs s3://website.s.croquis.com/devblog --delete --cache-control max-age=0
+AWS_PROFILE=croquis
+
+aws --profile $AWS_PROFILE sts get-caller-identity > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  aws sso login --profile $AWS_PROFILE
+fi
+
+aws --profile $AWS_PROFILE s3 sync docs s3://kakaostyle.in/website/devblog --delete --cache-control max-age=0
